9f7afddde756ef38e87c4bf62eebb05e93c7aef3
Manuel garcia <manuel.garcia999@hotmail.com>
2024-12-17 18:04:47 +0100

    Primer ejercicio resuelto vdeio 1h 23m

---
+++ 00-holamundo.js
@@ -0,0 +1,18 @@
+console.log("hola javascript!")
+console.log('hola javascript¡¡')
+console.log(`hola javascript¡¡¡`)
+
+// esto es un comentario simple de una linea
+
+/* esto es un comentario mas largo
+de varias lineas que se abre en donde inicia el comentario
+y termina en donde lo cierro*/
+
+console.log(5)
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
